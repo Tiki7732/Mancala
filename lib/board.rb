69e50345708dbd43e73b912718e38fbbd54a3a29
Laurent Saidah <lsaidah@gmail.com>
2020-06-06 16:34:46 -0500

    finished make_move method and winner method

--- lib/board.rb
+++ lib/board.rb
@@ -1,9 +1,10 @@
 class Board
-  attr_accessor :cups
+  attr_accessor :cups, :name1, :name2
 
   def initialize(name1, name2)
     @cups = Array.new(14)
     place_stones
+    @name1, @name2 = name1, name2
   end
 
   def place_stones
@@ -20,10 +21,30 @@ class Board
   end
 
   def make_move(start_pos, current_player_name)
+    stone_count = cups[start_pos].count
+    cups[start_pos] = []
+    next_pos = (start_pos + 1) % cups.count
+    p1 = current_player_name == name1 ? true : false
+    stone_count.times do 
+      if p1
+        next_pos = 0 if next_pos == 13
+      else
+        next_pos = 7 if next_pos == 6 
+      end
+      cups[next_pos].push(:stone)
+      next_pos = (next_pos + 1) % cups.count 
+    end
+    ending_cup_idx = next_pos - 1 unless next_pos == 0
+    ending_cup_idx = 13 if next_pos == 0
+    self.render
+    next_turn(ending_cup_idx)
+        
   end
 
   def next_turn(ending_cup_idx)
-    # helper method to determine whether #make_move returns :switch, :prompt, or ending_cup_idx
+   return :prompt if ending_cup_idx == 6 || ending_cup_idx == 13
+   return :switch if cups[ending_cup_idx].count == 1
+   return ending_cup_idx 
   end
 
   def render
@@ -35,9 +56,18 @@ class Board
   end
 
   def one_side_empty?
+    return true if cups[0..5].all?{|cup| cup.empty?}
+    return true if cups[7..12].all?{|cup| cup.empty?}
+    false
   end
 
   def winner
+    p1_score = cups[6].count
+    p2_score = cups[13].count
+
+    return :draw if p1_score == p2_score
+    winner = p1_score > p2_score ? name1 : name2
+    return winner
   end
 end
 
